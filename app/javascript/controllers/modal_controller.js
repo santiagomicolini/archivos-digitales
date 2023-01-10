6a6610294cb056357f104246e33e563d3573c9aa
camila modena <itscamilla@hotmail.com>
2023-01-10 23:30:59 +0100

    carousel in preview

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -19,8 +19,8 @@ export default class extends Controller {
 		// console.log(docs.slice(1))
 
 		// this.modalImgShellTarget.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' class='close' viewBox='0 0 20 20' fill='currentColor' data-action='click->modal#close'><path fill-rule='evenodd' d='M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z' clip-rule='evenodd' /></svg><iframe src=\"#\" class=\"object-center object-cover sm:rounded-md h-full w-full\" data-modal-target=\"modalImg\">";
-		this.modalImgShellTarget.innerHTML = `<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-			<div class="carousel-inner">
+		this.modalImgShellTarget.innerHTML = `<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" style="height: 100%; width: 580px;">
+			<div class="carousel-inner" style="padding: 0 60px;">
 				<div class="carousel-item active">
 					<img src="${JSON.parse(event.currentTarget.dataset.images)[0]}" class="d-block w-100" alt="...">
 				</div>
