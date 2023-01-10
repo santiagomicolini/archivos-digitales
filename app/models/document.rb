dd9172051a19a10ef530202ecc93ae19149d322e
Santiago Micolini <santi.micolini2@gmail.com>
2023-01-10 14:26:02 -0300

    search bar done

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,4 +3,12 @@ class Document < ApplicationRecord
   has_one_attached :file
   TAX_NAME = ["IVA", "SIJP", "SICORE", "ING. BRUTOS", "GANANCIAS"]
   AGENCY_NAME = ["RENTAS", "AFIP"]
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_name_and_agency,
+                  against: [ :description, :agency, :tax_name, :date ],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
