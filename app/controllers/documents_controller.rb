1473c8b3ff7f221ba24c8fd3f3ca81d595f8be2c
camila modena <itscamilla@hotmail.com>
2023-01-24 14:19:22 +0100

    preview done

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class DocumentsController < ApplicationController
   before_action :set_client, only: [:new]
   def index
@@ -16,6 +18,26 @@ class DocumentsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def download
+    pdf = Prawn::Document.new
+    pdf.text "hi"
+    send_data(pdf.render, filename: "archivo", type: "application/pdf")
+  end
+
+  def preview
+    @document = Document.find(params[:id])
+    pdf = Prawn::Document.new
+
+    params[:images][0..-2].each_with_index do |img, index|
+      doc_image = URI.open(img)
+      pdf.image doc_image, fit: [700, 700], position: :center, position: :center
+      # pdf.start_new_page unless params[:images][index + 1].nil?
+    end
+
+    # pdf.image doc_image, fit: [800, 800], position: :center, position: :center
+    send_data(pdf.render, filename: "#{@document.agency}_#{@document.tax_name}", type: "application/pdf", disposition: "inline")
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "/documents/:id/download", to: "documents#download", as: "document_download"
+  get "/documents/:id/preview/", to: "documents#preview", as: "document_preview"
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
 
   resources :clients, only: [:index, :show, :new, :create] do
     resources :documents, only: [:new, :create]
   end
-  resources :users, only: [:index]
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
